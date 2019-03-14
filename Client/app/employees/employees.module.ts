d4692ce7710a04ecf2b412342d1e1958d181cad3
Mark Graves <mark.graves@tylertech.com>
2019-03-14 15:32:57 -0500

    Completed implementation of list all employees tab

--- Client/app/employees/employees.module.ts
+++ Client/app/employees/employees.module.ts
@@ -5,7 +5,7 @@ import { EmployeesService } from './employees.service';
 import { employeesRouting } from './employees.routes';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule, MatButtonModule, MatInputModule, MatDividerModule, MatTabsModule, MatListModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+import { MatFormFieldModule, MatButtonModule, MatInputModule, MatDividerModule, MatTabsModule, MatDatepickerModule, MatNativeDateModule, MatCardModule } from '@angular/material';
 import { ListEmployeesComponent } from './list-employees/list-employees.component';
 import { EditEmployeesComponent } from './edit-employees/edit-employees.component';
 import { AddEmployeesComponent } from './add-employees/add-employees.component';
@@ -19,12 +19,12 @@ import { AddEmployeesComponent } from './add-employees/add-employees.component';
     MatInputModule,
     MatDividerModule,
     MatTabsModule,
-    MatListModule,
     MatDatepickerModule,
     MatNativeDateModule,
     FlexLayoutModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule
   ],
   declarations: [EmployeesComponent, ListEmployeesComponent, EditEmployeesComponent, AddEmployeesComponent],
   providers: [EmployeesService]
