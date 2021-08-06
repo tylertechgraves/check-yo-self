7577363bfae3c0ca00cb926b14c8a572dec72bd3
Mark Graves <mark.graves@tylertech.com>
2021-08-06 17:12:14 -0500

    chore: Added root path and updated paths to images

--- Startup.cs
+++ Startup.cs
@@ -60,7 +60,10 @@ namespace check_yo_self
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var rootPath = "/check-yo-self-markg";
+
             app.UseSwagger();
+            app.UsePathBase(rootPath);
 
             if (env.IsDevelopment())
             {
